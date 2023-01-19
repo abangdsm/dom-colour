4e99a05d03baa834fc637e8a94104c43e0a985a4
abangdsm <id.reversal@gmail.com>
2023-01-20 02:35:19 +0700

    Menambahkan tombol baru dari javascript

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const color = document.getElementById('ubahWarna');
 color.addEventListener('click', function() {
     document.body.classList.toggle('biru-muda');
 })
+
+const newBtn = document.createElement('button');
+const fnBtn = document.createTextNode('Acak Warna');
+newBtn.appendChild(fnBtn);
+newBtn.setAttribute('type', 'button');
+newBtn.after(color);
