7c3d7a81754f4c0a095121d4c9e7e28ada88c931
abangdsm <id.reversal@gmail.com>
2023-01-20 02:56:09 +0700

    Menambahkan fungsi auto background color secara random dengan JavaScript

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ color.addEventListener('click', function() {
     document.body.classList.toggle('biru-muda');
 })
 
-const newBtn = document.createElement('button');
-const fnBtn = document.createTextNode('Acak Warna');
-newBtn.appendChild(fnBtn);
-newBtn.setAttribute('type', 'button');
-newBtn.after(color);
+const btnRandom = document.createElement('button');
+const txtbtnRandom = document.createTextNode('Acak Warna');
+btnRandom.appendChild(txtbtnRandom);
+btnRandom.setAttribute('type', 'button');
+color.after(btnRandom);
+
+btnRandom.addEventListener('click', function() {
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+
+    document.body.style.backgroundColor = 'rgb('+r+', '+g+', '+b+')';
+})
